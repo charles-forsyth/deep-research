541ba3f371882a99e6bb32f4d1e67fb942133a91
charles-forsyth <forsythc@ucr.edu>
2025-12-13 09:49:47 -0500

    Bump version to v0.13.1

--- deep_research.py
+++ deep_research.py
@@ -58,7 +58,7 @@ user_db_path = os.path.join(xdg_config_home, "deepresearch", "history.db")
 load_dotenv(user_config_path)
 
 # Fallback version if not installed as a package
-__version__ = "0.13.0"
+__version__ = "0.13.1"
 
 def get_version():
     try:
